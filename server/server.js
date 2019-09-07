c6801d242377b40cd8fb6bc447a56350e07e62aa
JohnCFerguson <johnfe@netflix.com>
2019-09-07 00:27:30 -0600

    refresh page now works, updated res.sendFile to have ../dist/ as 2nd input

--- server/server.js
+++ server/server.js
@@ -20,7 +20,7 @@ app.use('/', express.static("./dist", {
 }));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, '/index.html'), (err) => {
+  res.sendFile(path.join(__dirname,'../dist/', '/index.html'), (err) => {
     if (err) {
       res.status(500).send(err);
     }
