f2beafa7b586a3bcba52f37e0314b91602e744d2
JohnCFerguson <snobord4life@gmail.com>
2019-10-04 11:30:04 -0600

    updated .gitignore and package.json and server.js for deployment to heroku

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const passport = require("passport");
 const users = require("./routes/api/users");
 const feedback = require("./routes/api/feedback");
 const app = express();
+const path = require("path");
 
 // Bodyparser middleware
 app.use(
@@ -13,6 +14,7 @@ app.use(
   })
 );
 app.use(bodyParser.json());
+
 
 // DB Config
 const db = require("./config/config").mongoURI;
@@ -38,6 +40,15 @@ require("./config/passport")(passport);
 // Routes
 app.use("/api/users", users);
 app.use("/api/feedback", feedback);
+
+if(process.env.NODE_ENV === 'production'){
+  // Set static
+  app.use(express.static("client/build"));
+
+  app.get('*', (req,res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  })
+}
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server up and running on port ${port} !`));
